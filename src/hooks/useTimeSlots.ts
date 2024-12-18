d364d741f09e1a489a5ec12ad1edd491975e3cf9
TirlaP <tirla.petru@yahoo.com>
2024-12-18 21:03:39 +0200

    Fix fuery problems

--- src/hooks/useTimeSlots.ts
+++ src/hooks/useTimeSlots.ts
@@ -1,3 +1,4 @@
+import { TIME_SLOTS_QUERY_KEY } from '@/constants/queryKeys'
 import { dentistService } from '@/services/dentist.service'
 import { TimeSlot } from '@/types'
 import { useQuery } from '@tanstack/react-query'
@@ -7,20 +8,21 @@ export const useTimeSlots = (dentistId: number, startDate: Date, endDate: Date) 
   const startDateStr = format(startDate, "yyyy-MM-dd'T'00:00:00.000'Z'")
   const endDateStr = format(endDate, "yyyy-MM-dd'T'23:59:59.999'Z'")
 
+  const queryKey = [TIME_SLOTS_QUERY_KEY, dentistId, startDateStr, endDateStr]
+
   const {
     data: availableSlots = [],
     isLoading,
     error,
   } = useQuery<TimeSlot[]>({
-    queryKey: ['timeSlots', dentistId, startDateStr, endDateStr],
+    queryKey,
     queryFn: () => dentistService.getAvailableSlots(dentistId, startDateStr, endDateStr),
-    staleTime: 5 * 60 * 1000, // Consider data fresh for 5 minutes
-    gcTime: 10 * 60 * 1000, // Keep in cache for 10 minutes before garbage collection
   })
 
   return {
     availableSlots,
     isLoading,
     error,
+    queryKey,
   }
 }
